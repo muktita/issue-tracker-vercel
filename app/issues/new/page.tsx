ba4ac30bfdb5128ec12714df47096026407959ac
muktita <muktitakim@gmail.com>
2024-07-02 14:00:35 -0700

    new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,10 @@
+import { TextField } from '@radix-ui/themes'
+import React from 'react'
+
+const NewIssuePage = () => {
+  return (
+    <TextField.Root placeholder="Title" />
+  )
+}
+
+export default NewIssuePage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Button } from '@radix-ui/themes'
+'use client';
 
-const IssuesPage = () => {
+import { Button, TextArea, TextField } from '@radix-ui/themes'
+import React from 'react'
+
+const NewIssuePage = () => {
   return (
-    <div><Button>New Issue</Button></div>
+    <div className='max-w-xl space-y-3'>
+      <TextField.Root placeholder='Title' />
+      <TextArea placeholder='Description' />
+      <Button>Submit New Issue</Button>
+    </div>
   )
 }
 
-export default IssuesPage
+export default NewIssuePage
