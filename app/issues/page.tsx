0f0282ddf7bad1ee2c5da8c54b568e8a28b0b9e6
muktita <muktitakim@gmail.com>
2024-07-02 15:17:48 -0700

    add spinner

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,52 +1,17 @@
-'use client';
+import React from 'react'
 
-import { Button, Callout, Text, TextArea, TextField } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from 'react-hook-form';
-import axios from 'axios';
-import "easymde/dist/easymde.min.css";
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '../validationSchemas';
-import { z } from 'zod';
-import ErrorMessage from '../components/ErrorMessage';
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
-type IssueForm = z.infer<typeof createIssueSchema>;
-
-const NewIssuePage = () => {
-  const router = useRouter();
-  const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
-    resolver: zodResolver(createIssueSchema)
-  
-  });
-  const [error, setError]= useState('');
+const IssuesPage = () => {
   return (
-    <div className='max-w-xl'>
-      {error && <Callout.Root color='red' className='mb-5'>
-        <Callout.Text>{error}</Callout.Text>  
-      </Callout.Root>}
-    <form className='space-y-3' 
-      onSubmit={handleSubmit(async (data) => {
-        try {
-          await axios.post('/api/issues', data);
-          router.push('/issues');
-        } catch (error) {
-          setError('An unexpected error occurred');
-        }
-      })}>
-    <TextField.Root placeholder='Title' {...register('title')}/>
-    <ErrorMessage>{errors.title?.message}</ErrorMessage>
-    <Controller
-        name="description"
-        control={control}
-        render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
-       />
-      <ErrorMessage>{errors.description?.message}</ErrorMessage>
-    <Button>Submit New Issue</Button>
-  </form>
-  </div>
-  )
-}
+    <div>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+    </div>
+  );
+};
 
-export default NewIssuePage
+
+export default IssuesPage;
