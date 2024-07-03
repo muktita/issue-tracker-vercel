17e6f3f940293ebb3ad81eba0da8e78ec8621c21
muktita <muktitakim@gmail.com>
2024-07-03 15:32:48 -0700

    add loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,7 @@
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
-import { Heading, Flex, Card, Text } from '@radix-ui/themes';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 interface Props {
@@ -22,6 +21,8 @@ const IssueDetailPage = async ({ params }: Props) => {
     if (!issue) {
         notFound();
     }
+
+
     return (
         <div>
             <Heading>{issue.title}</Heading>
