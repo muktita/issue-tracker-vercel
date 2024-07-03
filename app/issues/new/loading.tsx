4a56adc358fb56814da43677be0b40516b3dbfd4
muktita <muktitakim@gmail.com>
2024-07-03 15:46:05 -0700

    refactor organize imports statement

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,5 @@
+import Skeleton from '@/app/components/Skeleton'
 import { Box } from '@radix-ui/themes'
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingNewIssuePage = () => {
   return (
