1e851b9344fa1bf9df275506e6e6652e5869aff2
muktita <muktitakim@gmail.com>
2024-07-08 11:15:44 -0700

    add delete button and show confirmation

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/prisma/client";
-import { Box, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
 
 interface Props {
   params: {
@@ -22,12 +23,15 @@ const IssueDetailPage = async ({ params }: Props) => {
   }
 
   return (
-    <Grid columns={{ initial: "1", md: "2", lg: "3", xl:'4'}} gap="5">
-      <Box>
+    <Grid columns={{ initial: "1", sm: "5", lg: "6", xl:'5'}} gap="5">
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
       <Box>
+        <Flex direction='column' gap='4'>
        <EditIssueButton issueId={issue.id} />
+       <DeleteIssueButton issueId={issue.id} />
+       </Flex>
       </Box>
     </Grid>
   );
